012aab652dcafacbbca9889ba4234d3b0ade1a83
Glenn Turla <turlaglenn@gmail.com>
2024-03-13 02:23:55 +0800

    Glenn Turla
    finishing codes for main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,6 @@
 import 'package:flutter/material.dart';
-import 'loading_screen.dart';
+import 'dictionary_app.dart';
 
 void main() {
   runApp(DictionaryApp());
-}
-
-class DictionaryApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Dictionary App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: LoadingScreen(),
-    );
-  }
 }
