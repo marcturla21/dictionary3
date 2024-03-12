1cda339a9d594cc7a185632fcf47e75feb1709fa
marcturla39 <marcturla39@gmail.com>
2024-03-13 02:27:37 +0800

    *Marc Turla -added loading_screen.dart

--- lib/loading_screen.dart
+++ lib/loading_screen.dart
@@ -11,6 +11,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
   @override
   void initState() {
     super.initState();
+    // Simulate loading data for 5 seconds
     Timer(Duration(seconds: 5), () {
       Navigator.of(context).pushReplacement(
         MaterialPageRoute(
@@ -23,12 +24,13 @@ class _LoadingScreenState extends State<LoadingScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFFD7DAF3),
+      backgroundColor: Color(0xFFD7DAF3), // Set background color to #d7daf3
       body: Center(
         child: Image.network(
-          'https://i.redd.it/0g30oqtpiyd51.gif',
+          'https://i.redd.it/0g30oqtpiyd51.gif', // Replace with your GIF URL
           width: 200.0,
           height: 200.0,
+          // You can adjust width and height according to your preference
         ),
       ),
     );
