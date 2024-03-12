25769205bae8582adb94d28a3a9a7b857fcb0556
Glenn Turla <turlaglenn@gmail.com>
2024-03-13 02:14:49 +0800

    Glenn Turla
    finishing codes for main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
-// main.dart
-
-import 'dart:async';
 import 'package:flutter/material.dart';
+import 'loading_screen.dart';
 
 void main() {
   runApp(DictionaryApp());
@@ -17,40 +15,6 @@ class DictionaryApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: LoadingScreen(),
-    );
-  }
-}
-
-class LoadingScreen extends StatefulWidget {
-  @override
-  _LoadingScreenState createState() => _LoadingScreenState();
-}
-
-class _LoadingScreenState extends State<LoadingScreen> {
-  @override
-  void initState() {
-    super.initState();
-    // Simulate loading data for 5 seconds
-    Timer(Duration(seconds: 5), () {
-      Navigator.of(context).pushReplacement(
-        MaterialPageRoute(
-          builder: (context) => DictionaryScreen(),
-        ),
-      );
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color(0xFFD7DAF3),
-      body: Center(
-        child: Image.network(
-          'https://i.redd.it/0g30oqtpiyd51.gif',
-          width: 200.0,
-          height: 200.0,
-        ),
-      ),
     );
   }
 }
